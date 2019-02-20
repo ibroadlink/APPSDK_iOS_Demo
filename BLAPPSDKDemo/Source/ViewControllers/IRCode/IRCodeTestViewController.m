eee640824d2f4803bf663b3372d7a3a5fbe4b21b
hongkun.bai <hongkun.bai@broadlinik.com.cn>
2019-02-20 16:12:18 +0800

    红码修改

--- BLAPPSDKDemo/Source/ViewControllers/IRCode/IRCodeTestViewController.m
+++ BLAPPSDKDemo/Source/ViewControllers/IRCode/IRCodeTestViewController.m
@@ -26,9 +26,8 @@
 }
 @end
 
-@interface IRCodeTestViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface IRCodeTestViewController ()
 
-@property (weak, nonatomic) IBOutlet UITableView *testTableView;
 @property (nonatomic, strong) NSArray *testList;
 @property (nonatomic, strong) BLController *blcontroller;
 @property (nonatomic, strong) BLIRCode *blircode;
@@ -51,20 +50,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.testList = @[@"Recognize IRCode",
-                      @"AC IRCode",
-                      @"TV IRCode",
-                      @"STB IRCode",
-                      @"RM SubDevice"];
-    
     self.locateid = 27;
     self.providerid = 0;
     self.isleaf = 0;
     
     self.ircodeString = @"2600ca008d950c3b0f1410380e3a0d160e160d3b0d150e150e3910150d160d3a0f36101411380d150f3a0e390d3910370f150f38103a0d3a0e1211140f1411121038101310150f3710380e390e150f160d160e1410140f131113101310380e3b0f351137123611ad8e9210370f1511370e390f140f1410380f1311130f39101211130f390f380f150f390f1310380f3810380f380f141038103710380f1411121014101310380f14101310380f3810381013101311121014101211131014101310370f3910361138103710000d05";
     
-    self.testTableView.delegate = self;
-    self.testTableView.dataSource = self;
     
     self.blcontroller = [BLLet sharedLet].controller;
     self.blircode = [BLIRCode sharedIrdaCode];
@@ -78,53 +69,30 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.testList.count;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString* cellIdentifier = @"IRCODETESTCELL";
-    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
-    }
-    
-    cell.textLabel.text = self.testList[indexPath.row];
-    
-    return cell;
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    switch (indexPath.row) {
-        case 0:
+- (IBAction)button:(UIButton *)sender {
+    if (sender.tag == 100) {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"空调红外码选择" message:@"" preferredStyle:UIAlertControllerStyleActionSheet];
+        UIAlertAction *CateGories = [UIAlertAction actionWithTitle:@"品牌选择" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            self.devtype = BL_IRCODE_DEVICE_AC;
+            [self performSegueWithIdentifier:@"CateGoriesTableView" sender:nil];
+        }];
+        UIAlertAction *keyIdentify = [UIAlertAction actionWithTitle:@"红外码一键识别" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
             [self performSegueWithIdentifier:@"aKeyToIdentify" sender:nil];
-            break;
-        case 1:
-            _devtype = BL_IRCODE_DEVICE_AC;
-            [self performSegueWithIdentifier:@"CateGoriesTableView" sender:nil];
-            break;
-        case 2:
-            _devtype = BL_IRCODE_DEVICE_TV;
-            [self performSegueWithIdentifier:@"CateGoriesTableView" sender:nil];
-            break;
-        case 3:
-            _devtype = BL_IRCODE_DEVICE_TV_BOX;
-            [self selectSubAreaLocateid];
-            break;
-        case 4:
-            [self performSegueWithIdentifier:@"RMSubDevice" sender:nil];
-            break;
-        default:
-            break;
+        }];
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+        [alertController addAction:cancelAction];
+        [alertController addAction:CateGories];
+        [alertController addAction:keyIdentify];
+        [self presentViewController:alertController animated:YES completion:nil];
+    }else if (sender.tag == 101) {
+        _devtype = BL_IRCODE_DEVICE_TV;
+        [self performSegueWithIdentifier:@"CateGoriesTableView" sender:nil];
+    }else if (sender.tag == 102) {
+        _devtype = BL_IRCODE_DEVICE_TV_BOX;
+        [self selectSubAreaLocateid];
     }
-    
 }
+
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([segue.identifier isEqualToString:@"CateGoriesTableView"]) {
