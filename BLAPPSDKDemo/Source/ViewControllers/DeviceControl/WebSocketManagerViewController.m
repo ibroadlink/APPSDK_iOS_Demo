973b86a08438e2f82128c066861a0125d26ba37b
baihk <bhk545145@163.com>
2019-12-16 10:06:55 +0800

    长连接

--- BLAPPSDKDemo/Source/ViewControllers/DeviceControl/WebSocketManagerViewController.m
+++ BLAPPSDKDemo/Source/ViewControllers/DeviceControl/WebSocketManagerViewController.m
@@ -11,26 +11,24 @@
 #import <BLSWebScoket/BLSWebScoket.h>
 #import <BLSFamily/BLSFamily.h>
 @interface WebSocketManagerViewController ()<BLWebSocketDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *linkUrRLText;
 @property (weak, nonatomic) IBOutlet UITextView *resultView;
-@property (nonatomic, strong) BLSWebSocketClient *webSocket;
+@property (weak, nonatomic) IBOutlet UITextView *sendView;
+//@property (nonatomic, strong) BLSWebSocketClient *webSocket;
 @property (nonatomic, strong) NSMutableArray *familyIds;
+@property (nonatomic, copy) NSMutableDictionary *socketClients;
 @end
 
 @implementation WebSocketManagerViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //     使用WebSocket连接
-    BLSWebScoketUrlResult *urlResult = [[BLSWebSocketManager shareManager] apprelayGetUrl];
-    for (NSString *urlPath in urlResult.data.url) {
-        self.webSocket = [[BLSWebSocketManager shareManager] createLink:urlPath];
-        self.webSocket.delegate = self;
-    }
     [self getAllSceneInfo];
+    [BLSWebSocketManager shareManagerWithRelayType:@"share"];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
-    [self.webSocket closeWebSocketActively];
+    [[BLSWebSocketManager shareManager] destroyAllLink];
 }
 
 - (void)getAllSceneInfo {
@@ -61,9 +59,33 @@
         //场景状态取消订阅
         [self senceStatusUnSubscription:self.familyIds];
     }else if (sender.tag == 106) {
-        //查询设备推送订阅
+        //查询场景推送订阅
         [self senceStatusQuery];
+    }else if (sender.tag == 107) {
+        //查询长连接URL
+        BLSWebScoketUrlResult *urlResult = [[BLSWebSocketManager shareManager] apprelayGetUrl];
+        for (NSString *urlPath in urlResult.data.url) {
+            self.linkUrRLText.text = urlPath;
+        }
+    }else if (sender.tag == 108) {
+        //创建长连接
+        BLSWebSocketClient *webSocket = [[BLSWebSocketManager shareManager] createLink:self.linkUrRLText.text];
+        webSocket.delegate = self;
+        [self.socketClients setValue:[NSString stringWithFormat:@"%lu",(unsigned long)webSocket.hash] forKey:self.linkUrRLText.text];
+    }else if (sender.tag == 109) {
+        //发送长连接消息
+        NSString *sendString = self.sendView.text;
+        [[self getWebSocketClient] sendMsg:sendString];
+    }else if (sender.tag == 110) {
+        //销毁长连接
+        [[self getWebSocketClient] closeWebSocketActively];
     }
+}
+
+- (BLSWebSocketClient *)getWebSocketClient {
+    NSString *hash = [self.socketClients objectForKey:self.linkUrRLText.text];
+    BLSWebSocketClient *webSocket = [[BLSWebSocketManager shareManager] getWebSocketClientWithHash:hash];
+    return webSocket;
 }
 
 - (void)didInitMessage:(id)message {
@@ -83,7 +105,9 @@
 }
 
 - (void)didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean {
-    
+    if ([reason isKindOfClass:[NSString class]]) {
+        self.resultView.text = reason;
+    }
 }
 
 
@@ -114,7 +138,11 @@
     };
        
     NSString *sendString = [tempDict BLS_modelToJSONString];
-    [self.webSocket sendMsg:sendString];
+    self.sendView.text = sendString;
+    
+    NSString *hash = [self.socketClients objectForKey:self.linkUrRLText.text];
+    BLSWebSocketClient *webSocket = [[BLSWebSocketManager shareManager] getWebSocketClientWithHash:hash];
+    [webSocket sendMsg:sendString];
 }
 
 
@@ -144,7 +172,9 @@
     };
        
     NSString *sendString = [tempDict BLS_modelToJSONString];
-    [self.webSocket sendMsg:sendString];
+    self.sendView.text = sendString;
+    
+    [[self getWebSocketClient] sendMsg:sendString];
 }
 
 //查询设备推送订阅列表
@@ -158,7 +188,9 @@
     };
        
     NSString *sendString = [tempDict BLS_modelToJSONString];
-    [self.webSocket sendMsg:sendString];
+    self.sendView.text = sendString;
+
+    [[self getWebSocketClient] sendMsg:sendString];
 }
 
 //场景状态订阅
@@ -182,7 +214,9 @@
     };
        
     NSString *sendString = [tempDict BLS_modelToJSONString];
-    [self.webSocket sendMsg:sendString];
+    self.sendView.text = sendString;
+    
+    [[self getWebSocketClient] sendMsg:sendString];
 }
 
 
@@ -207,7 +241,9 @@
     };
        
     NSString *sendString = [tempDict BLS_modelToJSONString];
-    [self.webSocket sendMsg:sendString];
+    self.sendView.text = sendString;
+    
+    [[self getWebSocketClient] sendMsg:sendString];
 }
 
 //查询场景状态订阅列表
@@ -221,7 +257,9 @@
     };
        
     NSString *sendString = [tempDict BLS_modelToJSONString];
-    [self.webSocket sendMsg:sendString];
+    self.sendView.text = sendString;
+    
+    [[self getWebSocketClient] sendMsg:sendString];
 }
 
 - (NSMutableArray *)familyIds {
@@ -229,5 +267,12 @@
         _familyIds = [[NSMutableArray alloc] init];
     }
     return _familyIds;
+}
+
+- (NSMutableDictionary *)socketClients {
+    if (!_socketClients) {
+        _socketClients = [[NSMutableDictionary alloc] init];
+    }
+    return _socketClients;
 }
 @end
